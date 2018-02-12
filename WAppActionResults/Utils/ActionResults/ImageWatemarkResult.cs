c411931fb32b9fbea63441ee8b122375d5393fc9
Michael Emir <synmreynos@crecer.com.sv>
2018-02-12 17:07:14 -0600

    Agregar archivos de proyecto.

---
+++ WAppActionResults/Utils/ActionResults/ImageWatemarkResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace WAppActionResults.Utils.ActionResults
+{
+    public class ImageWatemarkResult
+    {
+
+    }
+}
